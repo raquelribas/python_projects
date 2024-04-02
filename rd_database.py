7c830cc11c1b76218787bf919ca6e4734b4bd334
Raquel Ribas <raqlast@gmail.com>
2024-04-01 21:23:39 -0300

    api_crm.py

--- rd_database.py
+++ rd_database.py
@@ -9,16 +9,20 @@ import json
 # API credentials
 client_id = "your_id"
 client_secret = "your_secret"
-today = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-today2 = datetime.datetime.today().strftime("%Y-%m-%d")
+today = datetime.datetime.today()
+today_param = today.strftime("%Y-%m-%d")
+week_ago = today - datetime.timedelta(days=6)
+week_ago_param = week_ago.strftime("%Y-%m-%d")
+current_month = today.strftime("%B")
+current_year = today.strftime("%Y")
 
 def read_access_file():
-    with open(r"C:\Users\admin\Desktop\Python\Codes\rd_tokens.json","r") as json_file:
+    with open(r"C:\path\file.json","r") as json_file:
         jsonData = json.load(json_file)
     return jsonData
 
 def overwrite_access_file(json_access):
-    with open(r"C:\Users\admin\Desktop\Python\Codes\rd_tokens.json","w") as json_file:
+    with open(r"C:\path\file.json","w") as json_file:
         json.dump(json_access, json_file)
 
 def generate_token(id, secret, refresh):
@@ -40,10 +44,9 @@ def generate_token(id, secret, refresh):
     r = response.json()
     return r["access_token"], r["refresh_token"]
 
-# list of all emails
+# info of all emails
 def get_emails(access_token):
 
-    print(f"Executando get_emails {today}")
     headers = {
         "accept": "application/json",
         "authorization": f"Bearer {access_token}"
@@ -74,140 +77,14 @@ def get_emails(access_token):
         for item in response['items']:
             #item.pop('links')
             emails.append(item)
-    print(f"get_emails concluída! {today}")
+    
 
     return emails
 
-# Lista de todos os uuid na segmentação 'Todos os contatos da Base de Leads'
-df = pd.read_excel(r"G:\path\file.xlsx", sheet_name=0)
-list_uuid = df['uuid'].tolist()
-
-
-# Detalhes de todos os contatos da base - não executar sempre!
-def get_contact_details(access_token, list_uuid):
-
-    print(f"Executando get_contact_details {today}")    
-    contact_details = []
-
-    for uuid in list_uuid:
-
-        url = f"https://api.rd.services/platform/contacts/uuid:{uuid}"
-
-        headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {access_token}"
-        }
-
-        response = requests.get(url, headers=headers)
-        contact_details.append(response.json())
-    print(f"get_contact_details concluída! {today}")
-
-    return contact_details
-
-def get_funil_contato(access_token, list_uuid):
-
-    print(f"Executando get_funil_contato {today}")
-    contact_funil = []
-
-    for uuid in list_uuid:
-
-        url = f"https://api.rd.services/platform/contacts/uuid:{uuid}/funnels/default"
-
-        headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {access_token}"
-        }
-
-        response = requests.get(url, headers=headers)
-        contact_funil.append(response.json())
-    print(f"get_funil_contato concluída! {today}")
-
-    return contact_funil
-
-def get_contact_events(access_token, list_uuid):
-
-    print(f"Executando get_contact_events {today}")    
-    contact_events = []
-
-    for uuid in list_uuid:
-        url = f"https://api.rd.services/platform/contacts/{uuid}/events"
-
-        headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {access_token}"
-        }
-
-        params = {
-            "event_type": ["CONVERSION", "OPPORTUNITY"]
-        }
-
-        response = requests.get(url, headers=headers)
-        contact_events.append(response.json())
-    print(f"get_contact_events concluída! {today}")
-
-    return response.json()
-
-def get_fluxoauto(access_token):
-
-    print(f"Executando get_fluxoauto {today}") 
-    headers = {
-        "accept": "application/json",
-        "authorization": f"Bearer {access_token}"
-    }
-
-    fluxo_auto = []
-    max_pages = 1
-    page = 1
-    url = f"https://api.rd.services/platform/workflows?page={page}&page_size=125"
-
-    response = requests.get(url, headers=headers)
-    max_pages = int(response.headers['pagination-total-pages'])
-    response = response.json()
-    page +=1
-
-    for item in response['workflows']:
-        #item.pop('links')
-        fluxo_auto.append(item)
-            
-    
-    while page <= max_pages:
-        url = f"https://api.rd.services/platform/workflows?page={page}&page_size=125"
-        response = requests.get(url, headers=headers)
-        response = response.json()
-
-        page +=1
-        
-        for item in response['workflows']:
-            #item.pop('links')
-            fluxo_auto.append(item)
-    print(f"get_fluxoauto concluída! {today}")
-
-    return fluxo_auto
-
-
-def get_fluxoauto_details(access_token, list_fluxoid):
-
-    print(f"Executando get_fluxoauto_details {today}")
-    fluxoauto_details = []
-
-    for id in list_fluxoid:
-
-        url = f"https://api.rd.services/platform/workflows/{id}"
-
-        headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {access_token}"
-        }
-
-        response = requests.get(url, headers=headers)
-        fluxoauto_details.append(response.json())
-    print(f"get_fluxoauto_details concluída! {today}")
-
-    return fluxoauto_details
-
+# analytics for email_mkt: open, clicks, etc.
 def get_emailmkt_analytics(access_token):
 
-    url = f"https://api.rd.services/platform/analytics/emails?start_date=2022-01-01&end_date={today2}"
+    url = f"https://api.rd.services/platform/analytics/emails?start_date=2022-01-01&end_date={today_param}"
 
     headers = {
         "accept": "application/json",
@@ -219,9 +96,10 @@ def get_emailmkt_analytics(access_token):
 
     return emailmkt['emails']
 
+# analytics for workflow emails: open, clicks, etc.
 def get_fluxo_analytics(access_token):
 
-    url = f"https://api.rd.services/platform/analytics/workflow_emails?start_date=2022-01-01&end_date={today2}"
+    url = f"https://api.rd.services/platform/analytics/workflow_emails?start_date={week_ago_param}&end_date={today_param}"
 
     headers = {
         "accept": "application/json",
@@ -233,23 +111,10 @@ def get_fluxo_analytics(access_token):
 
     return fluxo['workflow_email_statistics']
 
-def get_funil_analytics(access_token):
-
-    url = f"https://api.rd.services/platform/analytics/funnel?start_date=2023-01-01&end_date={today2}&grouped_by=weekly"
-
-    headers = {
-        "accept": "application/json",
-        "authorization": f"Bearer {access_token}"
-    }
-
-    response = requests.get(url, headers=headers)
-    funil = response.json()
-
-    return funil['funnel']
-
+# analytics for landing page coversion: visitors vs leads
 def get_conversion_analytics(access_token):
 
-    url = f"https://api.rd.services/platform/analytics/conversions?start_date=2023-01-01&end_date={today2}"
+    url = f"https://api.rd.services/platform/analytics/conversions?start_date={week_ago_param}&end_date={today_param}"
 
     headers = {
         "accept": "application/json",
@@ -261,9 +126,9 @@ def get_conversion_analytics(access_token):
 
     return conversion['conversions']
 
+# info of all landing pages
 def get_landingpage(access_token):
 
-    print(f"Executando get_landingpage {today}") 
     headers = {
         "accept": "application/json",
         "authorization": f"Bearer {access_token}"
@@ -293,8 +158,6 @@ def get_landingpage(access_token):
         for item in response:
             landing_page.append(item)
 
-    print(f"get_landingpage concluída! {today}")
-
     return landing_page
 
 
@@ -313,32 +176,24 @@ except:
     overwrite_access_file(read_json)
 
 df = pd.DataFrame(emails_response)
-df.to_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\Emails.xlsx", index=False)
-
-'''fluxo_auto_response = get_fluxoauto(access_token)
-
-df = pd.DataFrame(fluxo_auto_response)
-df.to_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\FluxoAUTO.xlsx", index=False)
-
-df = pd.read_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\FluxoAUTO.xlsx", sheet_name=0)
-list_fluxoid = df['id'].tolist()
-
-fluxoauto_details_response = get_fluxoauto_details(access_token, list_fluxoid)
-
-df = pd.DataFrame(fluxoauto_details_response)
-df.to_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\FluxoAUTO_details.xlsx", index=False)'''
+df.to_excel(r"G:\path\Emails.xlsx", index=False)
 
 emailmkt_analytics_response = get_emailmkt_analytics(access_token)
 
 df = pd.DataFrame(emailmkt_analytics_response)
-df.to_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\EmailMKT_analytics.xlsx", index=False)
+df.to_excel(r"G:\path\EmailMKT_analytics.xlsx", index=False)
 
-funil_analytics_response = get_funil_analytics(access_token)
+fluxo_analytics_response = get_fluxo_analytics(access_token)
 
-df = pd.DataFrame(funil_analytics_response)
-df.to_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\Funil_analytics.xlsx", index=False)
+df = pd.DataFrame(fluxo_analytics_response)
+df.to_excel(fr"G:\path\{current_year}\{current_month}\Fluxo_analytics_{today_param}.xlsx", index=False)
+
+conversion_analytics_response = get_conversion_analytics(access_token)
+
+df = pd.DataFrame(conversion_analytics_response)
+df.to_excel(fr"G:\Drives compartilhados\Spinoff Drive\RD_Database\Landing_analytics\{current_year}\{current_month}\Conversion_analytics_{today_param}.xlsx", index=False)
 
 landingpage_response = get_landingpage(access_token)
 
 df = pd.DataFrame(landingpage_response)
-df.to_excel(r"G:\Drives compartilhados\Spinoff Drive\RD_Database\landing_page.xlsx", index=False)
+df.to_excel(r"G:\path\landing_page.xlsx", index=False)
